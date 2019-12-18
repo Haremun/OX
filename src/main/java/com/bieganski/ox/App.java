e4d41f621754fabf1091e6de599d9c1924af2c88
Kamil <kamilbieg96@gmail.com>
2019-12-18 10:53:51 +0100

    initialize commit

---
+++ src/main/java/com/bieganski/ox/App.java
@@ -0,0 +1,7 @@
+package com.bieganski.ox;
+
+public class App{
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+}
---
+++ src/test/java/com/bieganski/ox/AppTest.java
@@ -0,0 +1,8 @@
+package com.bieganski.ox;
+
+import org.testng.annotations.Test;
+
+@Test
+public class AppTest{
+
+}
