43abbda27c81714a1386f120a17bd353a3f777a1
Kamil <kamilbieg96@gmail.com>
2019-12-30 15:42:03 +0100

    Logger functionality
    Added slf4j logger. Now app stores logs in file

--- src/main/java/com/bieganski/ox/App.java
+++ src/main/java/com/bieganski/ox/App.java
@@ -1,14 +1,22 @@
 package com.bieganski.ox;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 class App {
+    public static final Logger LOG = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+
+        LOG.info("Application started");
+
         Scanner scanner = new Scanner(System.in);
         UserInterface userInterface = new ConsoleInterface(scanner, System.out);
         userInterface.println("Tic tac toe!");
         //TODO: change const values ,create game setup
-        GameFlow gameFlow = new GameFlow(userInterface, 200);
+        GameFlow gameFlow = new GameFlow(userInterface, Integer.parseInt(args[0]));
         new Thread(gameFlow).start();
 
     }
