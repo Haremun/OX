68864c1740410be5777084f8585b939901f42aa8
Kamil <kamilbieg96@gmail.com>
2020-01-09 04:31:19 +0100

    Not working reverse

--- auto/createRSlant.sh
+++ auto/createRSlant.sh
@@ -7,30 +7,70 @@ function move {
  #ruch X i O
  for (( k=0; k<$WIN_SIZE; k++ ))
  do
-   TMP="$TMP$(($3 + $k)) $2."
-   second=$(($2 + 1))
-   if [ $second == $1 ]
+	 TMP="$TMP$(($3 + $2 + $k)) $(($2 - $k))."
+   row=$(($3 + $k + $2 + 1))
+   if [ $row == $1 ]
    then
-     second=0
+     row=0
    fi
    if [ $(($k+1)) != $WIN_SIZE ]
    then
-     TMP="${TMP}$k $second."
+	   TMP="${TMP}$row $(($3 + $k))."
+   fi
+ done
+ TMP=`echo $TMP | sed 's/\.$//g'`
+ echo $TMP
+}
+function moveColumn {
+ TMP="$1.3.1."
+ WIN_SIZE=3
+ #ruch X i O
+ for (( k=0; k<$WIN_SIZE; k++ ))
+ do
+	 TMP="$TMP$(($3 + $k)) $(($1 + $3))."
+   row=$(($3 + $k + $2 + 1))
+   if [ $row == $1 ]
+   then
+     row=0
+   fi
+   if [ $(($k+1)) != $WIN_SIZE ]
+   then
+	   TMP="${TMP}$row $(($3 + $k + $2))."
    fi
  done
  TMP=`echo $TMP | sed 's/\.$//g'`
  echo $TMP
 }
 function hor {
-  WIN_ROW=$(($1 - 3 + 1))
-  #ilosc row
-  for (( i = 0; i<$1; i++ ))
+  #ilosc wierszy
+  for (( i = $(($1 - 1)); i>0; i-- ))
   do
+    WIN_ROW=$(($1 - 3 - i))
+    if [ $WIN_ROW < 0 ]
+    then
     #ilosc wygranych w row
-    for (( z = 0; z<$WIN_ROW; z++ ))
+    for (( z = $WIN_ROW; z<0; z++ ))
     do
       move $1 $i $z
     done
+    else
+      break
+    fi
+  done
+  #ilosc kolumn
+  for (( i = $(($1 - 2)); i<0; i-- ))
+  do
+    WIN_COLUMN=$(($1 - 3 - i))
+    if [ $WIN_COLUMN < 0 ]
+    then
+    #ilosc wygranych w row
+    for (( z = $WIN_COLUMN; z<0; z++ ))
+    do
+      moveColumn $1 $i $z
+    done
+    else
+      break
+     fi
   done
 }
 
