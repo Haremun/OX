8cd5a5a7d2b1a40559a186cdc32606eaadd8607c
Kamil <kamilbieg96@gmail.com>
2020-01-09 04:01:48 +0100

    Slant

--- auto/runAll.sh
+++ auto/runAll.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 SIZE=${1:-4}
 IFS=$'\n'
-echo "" > horizontal.txt
 
 for cmd in $(./createHorizontal.sh $SIZE)
 do
-	./expect.sh "$cmd" >> horizontal.txt
+	./expect.sh "$cmd" >> results/horizontal.txt
 	echo .
-	echo "" >> horizontal.txt
-	echo "____________________" >> horizontal.txt
+	echo "" >> results/horizontal.txt
+	echo "____________________" >> results/horizontal.txt
 done
 for cmd in $(./createVertical.sh $SIZE)
 do
-	./expect.sh "$cmd" >> vertical.txt
+	./expect.sh "$cmd" >> results/vertical.txt
 	echo .
-	echo "" >> vertical.txt
-	echo "____________________" >> vertical.txt
+	echo "" >> results/vertical.txt
+	echo "____________________" >> results/vertical.txt
+done
+for cmd in $(./createSlant.sh $SIZE)
+do
+	./expect.sh "$cmd" >> results/slant.txt
+	echo .
+	echo "" >> results/slant.txt
+	echo "____________________" >> results/slant.txt
+done
+for cmd in $(./createRSlant.sh $SIZE)
+do
+	./expect.sh "$cmd" >> results/reverseSlant.txt
+	echo .
+	echo "" >> results/reverseSlant.txt
+	echo "____________________" >> results/reverseSlant.txt
 done
